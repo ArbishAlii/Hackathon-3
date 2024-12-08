82a0afdd542326c7ba9474a25affbf1a040bf296
arbishalii <arbish210@gmail.com>
2024-12-09 01:28:31 +0500

    hackathon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,13 @@
 import Image from "next/image";
+import Hero from "@/components/Hero";
+
+import Product from '../components/Product';
+import JoinUs from '../components/JoinUs';
+import Login from '../components/Login';
+import Contact from '../components/Contact';
+import Button from '../components/Button';
+
+
 
 export default function Home() {
   return (
@@ -28,6 +37,12 @@ export default function Home() {
         </div>
       </div>
 
+      <Hero />
+      <JoinUs />
+      <Contact />
+      <Login />
+      <Product />
+      {/* <Button /> */}
       <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
